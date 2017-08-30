1b7f54f7877b4995205df0ebcd156bc947648bec
Artur Oganesyan <artur.og@yahoo.com>
2017-08-30 02:07:48 -0500

    Search Box Container
    SearchBox container and current user location adding

--- src/routes/Home/components/MapContainer/index.js
+++ src/routes/Home/components/MapContainer/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View } from 'native-base';
 import MapView from 'react-native-maps';
 
+import SearchBox from '../SearchBox';
+
 import styles from './MapContainerStyles.js'
 
 export const MapContainer = ({region}) => {
@@ -18,6 +20,7 @@ export const MapContainer = ({region}) => {
 				pinColor= 'green'
 				/>
 			</MapView>
+			<SearchBox />
 		</View>
 		)
 }
